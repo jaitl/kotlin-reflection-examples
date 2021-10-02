31ae5e39e4accc24a40d96e9bbbdb27025ea8446
Jaitl <jaitl@outlook.com>
2021-10-02 22:42:35 +0300

    update refined test

--- examples/src/test/kotlin/pro/jaitl/kotlin/reflection/RefinedFunctionTest.kt
+++ examples/src/test/kotlin/pro/jaitl/kotlin/reflection/RefinedFunctionTest.kt
@@ -1,11 +1,34 @@
 package pro.jaitl.kotlin.reflection
 
+import kotlin.reflect.KClass
 import kotlin.test.Test
 
 class RefinedFunctionTest {
+
+    fun printType(clazz: KClass<*>) {
+        println(clazz.qualifiedName)
+    }
+
+    inline fun <reified T>printType() {
+        println(T::class.qualifiedName)
+    }
+
+    inline fun <reified T>printType(obj: T) {
+        println(T::class.qualifiedName)
+    }
+
     @Test
-    fun testRefined() {
+    fun testSentKTypeAsArgument() {
         printType(String::class)
+    }
+
+    @Test
+    fun tesGetKTypeFromRefinedParam() {
         printType<String>()
+    }
+
+    @Test
+    fun tesGetKTypeFromArgument() {
+        printType("test")
     }
 }
